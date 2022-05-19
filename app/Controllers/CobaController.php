99986c18ae78f3311f4c953f79aca2a724d47658
khamimq <khamim.q@gmail.com>
2022-05-19 09:13:07 +0700

    coba upload

---
+++ app/Controllers/CobaController.php
@@ -0,0 +1,11 @@
+<?php 
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class CobaController extends Controller{
+    public function index()
+    {
+        return view('coba2');
+    }
+}
